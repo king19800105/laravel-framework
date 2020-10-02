5a5b1cf55739617f1db96031fc83684819af2d63
Anthony <king19800105@gmail.com>
2020-10-03 01:07:10 +0800

    去掉idea文件

--- app/Repositories/Eloquent/AdminRepositoryEloquent.php
+++ app/Repositories/Eloquent/AdminRepositoryEloquent.php
@@ -57,7 +57,7 @@ class AdminRepositoryEloquent extends BaseRepository implements AdminRepository
                 ->syncRoles([])
                 ->delete();
         });
-        
+
         return true;
     }
 
