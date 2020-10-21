8ead1c4f40f79406f8483f5e3cc4a7d1e7c62ba9
Anthony <king19800105@gmail.com>
2020-10-21 23:15:45 +0800

    修复hash的bug

--- app/Providers/ComponentServiceProvider.php
+++ app/Providers/ComponentServiceProvider.php
@@ -5,9 +5,10 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-use App\Components\{
-    File\FileHandler,
+use App\Components\{File\FileHandler,
     File\LocalFileHandler,
+    Hash\HashHandler,
+    Hash\IHash,
     Local\LocalCache,
     Local\APCuLocalCache,
     Queue\QueueClient,
@@ -25,5 +26,6 @@ class ComponentServiceProvider extends ServiceProvider
         $this->app->singleton(LocalCache::class, APCuLocalCache::class);
         $this->app->singleton(QueueClient::class, RedisQueueClient::class);
         $this->app->singleton(FileHandler::class, LocalFileHandler::class);
+        $this->app->singleton(IHash::class, HashHandler::class);
     }
 }
