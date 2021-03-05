a72a42fdaab887254a1bc2da6bd30acdd5edd3ed
Anthony <king19800105@gmail.com>
2021-03-05 23:33:42 +0800

    限流器

--- app/Traits/QPSRateLimiter.php
+++ app/Traits/QPSRateLimiter.php
@@ -23,13 +23,13 @@ trait QPSRateLimiter
     protected string $rateLimiterKey = 'qps:limit:%s:%s';
 
     /**
-     * 校验是否通过
+     * 校验是否触发限流
      *
      * @param string $apiName 接口名称或标识
      * @param string $restriction 约束值可以是用户id、用户ip、或id+ip的组合
      * @param int $qps 每秒的请求qps量级
      * @param int $millisecond 毫秒过期时间
-     * @return bool true表示通过、false表示触发限流
+     * @return bool true触发限流、false表示通过
      */
     public function limit(string $apiName, string $restriction, int $qps = 3, int $millisecond = 1050): bool
     {
